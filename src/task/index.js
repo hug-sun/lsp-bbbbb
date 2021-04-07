efcf0a2babe76d747d0f637ec8ad6ce9b8f8fc15
cuixiaorui <cui_xiaorui@126.com>
2021-04-07 16:08:15 +0800

    feat: add share video task

--- src/task/index.js
+++ src/task/index.js
@@ -1,5 +1,7 @@
 // task 的机制可以组合，比如每日任务就可以包含3个小任务 daily -> [ 登录，观看视频，投币 ]
+// todo 先实现一个任务把 比如自动投币 -> 把每日投币的逻辑移植过来
 const { execute: coin } = require("./coin");
+const { execute: shareVideo } = require("./shareVideo");
 
 const tasks = [];
 
@@ -8,7 +10,8 @@ function addTask(task) {
 }
 
 // 注册 task
-addTask(coin);
+// addTask(coin);
+addTask(shareVideo);
 
 exports.start = async function start() {
   // 并行的执行所有的任务
---
+++ src/task/shareVideo.js
@@ -0,0 +1,53 @@
+const { getCsrf } = require("../user");
+const { post, get } = require("../request");
+// 分享视频
+exports.execute = async () => {
+  if (await isShare()) {
+    console.log("今日已经分享过视频了");
+    return;
+  }
+  share(getShareVideoAid());
+};
+
+async function share(aid) {
+  const res = await post("https://api.bilibili.com/x/web-interface/share/add", {
+    aid,
+    csrf: getCsrf(),
+  });
+
+  if (res.data.code === 0) {
+    console.log("每日任务分享视频成功✔");
+  } else {
+    console.log("每日任务分享视频失败❌");
+  }
+}
+
+async function isShare() {
+  const {
+    data: { share_av },
+  } = await getDailyTaskInfo();
+
+  return share_av;
+}
+
+async function getShareVideoAid() {
+  const archives = await getRecommendVideos(5, 1);
+  return archives[0].aid;
+}
+
+async function getDailyTaskInfo() {
+  const { data } = await get("https://account.bilibili.com/home/reward");
+  return data;
+}
+
+// 获取b 站推荐的视频
+// pageSize 为想要获取的视频数量
+// rId 是具体分区的 id -> TODO 可以后面用常量来表示分区 id
+async function getRecommendVideos(pageSize, rid) {
+  const res = await get(
+    "https://api.bilibili.com/x/web-interface/dynamic/region",
+    { ps: pageSize, rid }
+  );
+
+  return res.data.data.archives;
+}
