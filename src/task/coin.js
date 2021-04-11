b4892f4a4a5b389e18b77f3d827358b86cb90ec3
cuixiaorui <cui_xiaorui@126.com>
2021-04-11 11:48:05 +0800

    feat: add comments for coin

--- src/task/coin.js
+++ src/task/coin.js
@@ -3,14 +3,20 @@ const { getCsrf } = require("../user");
 // 投币
 exports.execute = async () => {
   // 视频id 的话，可以基于 up 主来获取随机的 视频 id
-  const aid = "801383587";
-  const res = await post("https://api.bilibili.com/x/web-interface/coin/add", {
-    aid,
-    // 投币的数量也可以控制，比如说在完成每日任务的时候，可以基于是否满足了任务来计算出来需要投币多少
-    multiply: 1,
-    select_like: 0,
-    csrf: getCsrf(),
-  });
+  // const aid = "801383587";
+  // const res = await post("https://api.bilibili.com/x/web-interface/coin/add", {
+  //   aid,
+  //   // 投币的数量也可以控制，比如说在完成每日任务的时候，可以基于是否满足了任务来计算出来需要投币多少
+  //   multiply: 1,
+  //   select_like: 0,
+  //   csrf: getCsrf(),
+  // });
 
-  console.log(res.data);
+  // console.log(res.data);
+
+
+
+  // 1. 今日需要投币的数量(投)
+  // 2. 可以投币的视频
+  // 3. 进行投币
 };
